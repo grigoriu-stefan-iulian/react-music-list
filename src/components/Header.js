0e42c77ad35c86b520128ec10770a2df75f945e3
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-03-06 18:40:14 +0200

    api call and result display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, TextField, Button } from '@material-ui/core'
+import { AppBar, 
+    Toolbar, 
+    Typography, 
+    TextField, 
+    Button, 
+    List,
+    ListItem, 
+    ListItemAvatar, 
+    ListItemText, 
+    Avatar } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles';
 
 //make class
 //take methods from app.js
-
-/*
-
-
-)*/
+//afisare eroare - homework
 
 const styles = theme => ({
     button: {
@@ -19,28 +24,52 @@ const styles = theme => ({
         display: 'none',
     },
 });
+
 const Header = ({ onTextChange, state, onSearchClick, classes }) => (
-    <header className='App-header'>
-        <AppBar position="static" color="inherit" />
-        <Toolbar>
-            <Typography variant="h6" color="inherit" />
-            <TextField
-                placeholder="Search on Spotify"
-                onChange={onTextChange}
-                value={state.searchText}
-            />
-            <Button
-                variant="contained"
-                color="secondary"
-                className={classes.button}
-                onClick={onSearchClick}
-                disabled={state.searchText.length === 0}
-            >       Search
+    <div className="container">
+        <header className='header'>
+            <AppBar position="static" color="inherit" />
+            <Toolbar>
+                <Typography variant="h6" color="inherit" />
+                <TextField
+                    placeholder="Search on Spotify"
+                    onChange={onTextChange}
+                    value={state.searchText}
+                />
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    className={classes.button}
+                    onClick={onSearchClick}
+                    disabled={state.searchText.length === 0}
+                >       Search
             </Button>
+                {this.state.searchText.length && (
+                    <Button
+                        onClick={() => this.setState({ searchText: '' })}
+                        variant="contained"
+                    >
+                        Clear
+                </Button>
+                )}
 
-        </Toolbar>
+            </Toolbar>
 
-    </header>
+        </header>
+        <List className="search__results">
+            {this.state.artists.map((artist, i) => {
+                return (<ListItem
+                    button
+                    key={i}
+                >
+                    <ListItemText primary={artist.name} />
+                     Views: {artist.listeners} 
+                </ListItem>)
+
+
+            })}
+        </List>
+    </div>
 );
 
 Header.propTypes = {
