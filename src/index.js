f1477ef9b879565c0b5a0007206cefd7cd32dd3d
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-04-19 19:31:42 +0300

    Material UI custom styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import MusicAppRouter from './routers/MusicAppRouter'
+import MaterialUITest from './playground/MaterialUI-playground'
 
-ReactDOM.render(<MusicAppRouter />, document.getElementById('root'));
+ReactDOM.render(<MaterialUITest />, document.getElementById('root'));
 
