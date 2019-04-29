6782045d614feeff4b670c191e2e701af30f7803
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-04-29 21:46:02 +0300

    fixed small error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/styles.scss'
 import MusicAppRouter from './routers/MusicAppRouter'
-import './firebase/firebase'
-import FirebaseRef from './playground/FirebaseRef'
+//import './firebase/firebase'
+//import FirebaseRef from './playground/FirebaseRef'
 
 ReactDOM.render(<MusicAppRouter />, document.getElementById('root'))
