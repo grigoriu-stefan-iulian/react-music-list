e048988c9b1b513432b2b28475fc748523187167
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-05-06 16:01:07 +0300

    refactor MusicAppRouter and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import ReactDOM from 'react-dom'
-import './styles/styles.scss'
 import MusicAppRouter from './routers/MusicAppRouter'
-//import './firebase/firebase'
-//import FirebaseRef from './playground/FirebaseRef'
+import MusifyContext from './context/musify-context'
+import MusifyReducer from './reducers/musify-reducer'
+import './styles/styles.scss'
 
-ReactDOM.render(<MusicAppRouter />, document.getElementById('root'))
+const App = () => {
+    const [savedArtists, dispatch] = useReducer(MusifyReducer, [])
+    const [artists, setArtists] = useState([])
+
+    useEffect(() => {
+        const existing = localStorage.getItem('savedArtists')
+        if (existing) {
+            dispatch({ type: "POPULATE_LIST", artists: JSON.parse(localStorage.getItem('savedArtists')) })
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('savedArtists', JSON.stringify(savedArtists))
+    }, [savedArtists])
+
+    return (
+        <MusifyContext.Provider value={{ savedArtists, dispatch, artists, setArtists }}>
+            <MusicAppRouter />
+        </MusifyContext.Provider>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
