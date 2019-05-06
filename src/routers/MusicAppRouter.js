b5018a2b678e2e521f3f464c3b2daa90a95eec80
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-05-06 16:01:07 +0300

    refactor MusicAppRouter and index.js

--- src/routers/MusicAppRouter.js
+++ src/routers/MusicAppRouter.js
@@ -1,34 +1,13 @@
-import React, { useEffect, useReducer, useState } from 'react'
+import React from 'react'
 import { HashRouter, Route, Switch } from "react-router-dom"
-//import createHistory from 'history/createBrowserHistory'
 import Header from '../components/Header'
 import FavoriteArtists from '../components/FavoriteArtists'
-import MusifyContext from '../context/musify-context'
-import MusifyReducer from '../reducers/musify-reducer'
 import ArtistList from '../components/ArtistList'
 import NotFoundPage from '../components/NotFoundPage'
 import Home from '../components/Home'
 import Footer from '../components/Footer'
 
-//const history = require("history").createBrowserHistory()
-
-const MusicAppRouter = () => {
-    const [savedArtists, dispatch] = useReducer(MusifyReducer, [])
-    const [artists, setArtists] = useState([])
-
-    useEffect(() => {
-        const existing = localStorage.getItem('savedArtists')
-        if (existing) {
-            dispatch({ type: "POPULATE_LIST", artists: JSON.parse(localStorage.getItem('savedArtists')) })
-        }
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('savedArtists', JSON.stringify(savedArtists))
-    }, [savedArtists])
-
-    return (
-        <MusifyContext.Provider value={{ savedArtists, dispatch, artists, setArtists }}>
+const MusicAppRouter = () => (
             <HashRouter>
                 <Header />
                 <Switch>
@@ -39,8 +18,6 @@ const MusicAppRouter = () => {
                 </Switch>
                 <Footer />
             </HashRouter>
-        </MusifyContext.Provider>
     )
-}
 
 export default MusicAppRouter
